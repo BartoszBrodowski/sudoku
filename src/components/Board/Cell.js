305de25b45c17b17c73af1fd7fe880e2b682ec89
Bartosz Brodowski <bartolini10058@gmail.com>
2022-06-18 23:48:36 +0200

    Fix validation logic
    Deleted a bug where on input of a number and then deleting it you couldn't input it again.
    Fixed it by adding board[row][column] = input in the changeCellValue function which changes the value in the board array.
    The problem was that even though the state was changing, the value in the board array was still the same
    therefore caused a problem as in other validation functions we check whether the input value is present in the board, not the state.

--- src/components/Board/Cell.js
+++ src/components/Board/Cell.js
@@ -7,51 +7,51 @@ const Cell = ({ row, column, board, active }) => {
 	const [cellValue, setCellValue] = useState(board[row][column]);
 
 	const changeCellValue = (e) => {
+		let input = e.target.value;
 		if (active) {
-			if (e.target.value === '') {
-				setCellValue(e.target.value);
+			if (input === '') {
+				setCellValue('0');
+				board[row][column] = input;
 			}
-			if (_.inRange(e.target.value, 1, 10)) {
-				if (!isInRow(e)) {
-					if (!isInColumn(e)) {
-						setCellValue(e.target.value);
-						board[row][column] = e.target.value;
+			if (_.inRange(input, 1, 10)) {
+				if (!isInRow(input)) {
+					if (!isInColumn(input)) {
+						setCellValue(input);
+						board[row][column] = input;
 						// console.log(e.target.value);
 						// console.log(e.target);
 					}
 					// console.log(value);
 				}
 			}
+			// isInSquare()
 		}
 	};
 
-	const isInRow = (e) => {
-		if (board[row].includes(e.target.value)) {
-			return true;
-		} else {
-			return false;
-		}
+	const isInRow = (num) => {
+		return board[row].includes(num);
 	};
 
-	const isInColumn = (e) => {
-		const columnArray = [];
+	const isInColumn = (num) => {
+		let columnArray = [];
 		for (let i = 0; i < board.length; i++) {
 			columnArray.push(board[i][column]);
 		}
-		for (let i = 0; i < columnArray.length; i++) {
-			if (e.target.value === columnArray[i]) {
-				return true;
+		return columnArray.indexOf(num) > -1;
+	};
+	// Checks if the number provided is already in a 3x3 square
+	// (the board consists of 9 3x3 squares)
+	const isInSquare = (num) => {
+		if (column % 3 === 0) {
+			if (row % 3 === 0) {
+				for (let i = 0; i < 3; i++) {
+					for (let j = 0; j < 3; j++) {
+						if (board[row + i][column + j] === num) {
+						}
+					}
+				}
 			}
 		}
-		return false;
-		// For each row check if the value
-		// is included in a column
-		// board.forEach((row) => {
-		// 	if (row[column] === e.target.value) {
-		// 		console.log(row[column]);
-		// 		return true;
-		// 	}
-		// });
 	};
 
 	return (
