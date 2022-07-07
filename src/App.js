d501f9b5b302337cf3e4957108c25c3f407cbcd4
Bartosz Brodowski <bartolini10058@gmail.com>
2022-07-07 09:41:20 +0200

    Add buttons section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Board from './components/Board/Board';
+import Buttons from './components/Buttons/Buttons';
+
 import styles from './styles/globals.module.scss';
 
 function App() {
 	return (
 		<div className={styles.app}>
 			<Board />
+			<Buttons />
 		</div>
 	);
 }
---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+
+const Buttons = () => {
+	return (
+		<div>
+			<Button text='Check' />
+			<Button text='Clear' />
+			<Button text='Solve' />
+		</div>
+	);
+};
+
+export default Buttons;
